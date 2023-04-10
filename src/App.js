7324aee3ff0613c3d8cff566c70f84c6ed7138c2
Shubham-Sharma <viku.vsk@gmail.com>
2023-04-10 22:20:10 +0530

    3.Assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const arr = ['Good Afternoon', 'Good Evening', 'Good Night', 'Happy Birthday','Good Afternoon', 'Good Evening', 'Good Night', 'Happy Birthday'];
+  const [state, setState] = useState('Good Morning');
+  const [count, setCount] = useState(0);
+
+  function update() {
+    setCount(count + 1);
+    setState(arr[count]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mainContainer'>
+      <div className='Container'>
+      <h1>Click on Button to see the greetings</h1>
+      <h2>{state}</h2>
+      <button onClick={update}>Click</button>
+      </div>
+      
     </div>
   );
 }
