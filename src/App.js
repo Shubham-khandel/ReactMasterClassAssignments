a3e7f96cba329eca2389d317572d20a1baa40bee
Shubham-Sharma <viku.vsk@gmail.com>
2023-04-10 22:03:55 +0530

    1.Assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  const [num , setNum] = useState(1)
+
+  const arr = [1,2,3,4,5,6,7,8,9,10]
+  const randomNum = Math.floor(Math.random()*100 + 1)
+  
+
+  function tablee(){
+    setNum(randomNum)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mainContainer'>
+      <div className='Container'>
+        <h1>Table of {num}</h1>
+      {
+        arr.map((x, index)=>{
+         return <ul key={index}>
+           <li>{`${x}${'.'}`}</li><li key={index}>{x*num}</li>
+          </ul>
+        })
+      }
+      <button  onClick={tablee}>Click</button>
+      </div>
+      
     </div>
   );
 }
