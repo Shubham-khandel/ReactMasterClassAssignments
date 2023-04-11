0f4dd8758f83bdbc153b05642d62cfcdbabcc00b
Shubham-Sharma <viku.vsk@gmail.com>
2023-04-11 21:16:26 +0530

    4.DataFetchingAssignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((dataa) => setData(dataa));
+  }, []);
+
+  function onClickData() {
+    setData([]);
+  }
+
+  function deletee(index) {
+    let newData = data.filter((elem, i) => {
+      return i != index;
+    });
+    setData(newData);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <table>
+        <thead>
+          <tr>
+            <th>Body</th>
+            <th>Id</th>
+            <th>Title</th>
+            <th>UserId</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((user, index) => {
+            return (
+              <tr key={index}>
+                <td>{user.body}</td>
+                <td>{user.id}</td>
+                <td>{user.title}</td>
+                <td>{user.userId}</td>
+                <span onClick={() => deletee(index)}>X</span>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <button onClick={onClickData}>Clear Data</button>
     </div>
   );
 }
